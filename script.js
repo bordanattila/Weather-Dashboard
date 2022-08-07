cb7d22c58d022a653383139d03c8e13134bac284
Attila Bordan <bordan_attila@hotmail.com>
2022-08-07 11:59:51 -0400

    finising display functions and adding api call

--- script.js
+++ script.js
@@ -1,42 +1,23 @@
-var lon = -76.342339;
-var lat = 37.028271;
-var exclude = ["current", "minutely", "daily"];
 var city_name = "Tonganoxie";
+var lat = 0;
+var lon = 0;
+var exclude = ["minutely", "hourly", "alerts"]
 var state_code;
 var country_code;
 var inputField = $("#lookingFor");
 var cityID;
-
-
-api_key = "";
-
-// fetch ("http://api.openweathermap.org/geo/1.0/direct?q="+city_name+"&appid="+api_key)
-//     .then(function (geocord) {
-//         return geocord.json();
-//     })
-//     .then(function (data) {
-//         console.log(data)
-//     });
-
-// fetch ("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&cnt=3&appid="+api_key)
-//     .then(function (response) {
-//         $("p.1").text(response.status)
-//         return response.json();
-//     })
-//     .then(function (weather) {
-//         console.log(weather)
-//     })
+api_key = "019e087c294ce22b349c23fe666c8451";
+var today = "";
 
 var checkHistory = function () {
     for (city in localStorage) {
         if (city.includes("city")) {
 
             var targetCity = "#"+city;
-            console.log(city)
-            // console.log(typeof(targetCity))
             var valueCity = localStorage.getItem(city);
             var history = $("#history");
-            var historyItem = $("<p>")
+            var historyItem = $("<button>")
+            historyItem.addClass("btn btn-secondary stretch margin")
             historyItem.text(valueCity)
             history.append(historyItem);
             historyItem.attr("id", targetCity)
@@ -49,18 +30,74 @@ var checkHistory = function () {
 
 checkHistory();
 
-function displayCity () {
+function getLongLat (city_name) {
+    //Add city name to get lat and long
+    console.log(city_name);
+    fetch ("http://api.openweathermap.org/geo/1.0/direct?q="+city_name+"&appid="+api_key)
+    .then(function (geocord) {
+        return geocord.json();
+    })
+    .then(function (coordinates) {
+            lat = (coordinates[0].lat);
+            lon = (coordinates[0].lon);
+            getWeather(lat, lon);
+        });
+    }
+    
+    // Add lat and long to get weather data
+    function getWeather (lat, lon) {
+        fetch ("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&units=metric&appid="+api_key)
+        .then(function (response) {        
+            return response.json();
+        })
+        .then(function (weather) {
+            console.log("weather");
+            console.log(weather);
+            var todaySplit = (weather.list[0].dt_txt).split(" ")
+            today = todaySplit[0]
+            temperatureShow = weather.list[0].main.temp
+            humidityShow = weather.list[0].main.humidity
+            windShow = weather.list[0].wind.speed
+            getIcon = weather.list[0].weather[0].icon
+            // icon = url("http://openweathermap.org/img/wn/"+getIcon+"@2x.png")
+            console.log(today)
+            displayCity(city_name);
+        })
+    }    
+
+var temperatureShow;
+var windShow;
+var humidityShow;
+var icon;
+
+function displayCity (city_name) {
+    var cityToShow = $("#yourResult");
+    cityToShow.text(city_name + " ( " + today + " ) " + icon);
+    var temperature = $("<p>");
+    temperature.text("Temperature: "+temperatureShow+ " C");
+    cityToShow.append(temperature)
+    var wind =$("<p>");
+    wind.text("Wind: "+windShow+" Km/h");
+    cityToShow.append(wind)
+    var humidity = $("<p>");
+    humidity.text("Humidity: "+humidityShow+" %");
+    cityToShow.append(humidity)
+}
+
+function displayCityAgain (searchedCitys) {
     var currentCity = $("#yourResult");
-    currentCity.text($(inputField).val())
+    currentCity.text(searchedCitys + " (" + "date)");
+    getLongLat(city_name)
 }
 
 function addTohistory () {
     var searchedCitys = $("#history");    
-    var newItem = $("<p>");
+    var newItem = $("<button>");
     var inputFieldValue = $(inputField).val();
     newItem.text(inputFieldValue);
+    newItem.addClass("btn btn-secondary stretch margin");
     searchedCitys.append(newItem);
-    $("p").attr("id", function(index) {
+    $("button").attr("id", function(index) {
         return "city"+index;
     })
     store(newItem, inputFieldValue)
@@ -74,6 +111,12 @@ function store (newItem, inputFieldValue) {
 
 $("#find").on("click", function (event) {
     event.preventDefault();
-    displayCity();
+    city_name = ($(inputField).val())
+    getLongLat(city_name)
     addTohistory();
+})
+
+$("button.btn-secondary").on("click", function () {
+    city_name = ($(this).text());
+    displayCityAgain(city_name);
 })
